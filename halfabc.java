4e38dd8145f88a8ec71d1a83e52384f122fcae53
malli24 <malleswari82006@gmail.com>
2024-12-06 14:37:06 +0530

    Add files via upload

---
+++ halfabc.java
@@ -0,0 +1,15 @@
+public class halfabc
+{
+    public static void main(String args[])
+    {
+        for(int i=5;i>=5;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+                  
+    }
+}
