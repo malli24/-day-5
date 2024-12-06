53c00c692ced67bd19167a88819dc68513a88813
malli24 <malleswari82006@gmail.com>
2024-12-06 14:33:21 +0530

    Add files via upload

---
+++ half3.java
@@ -0,0 +1,15 @@
+public class half3
+{
+    public static void main(String args[])
+    {
+        for(int i=5;i>=5;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+                  
+    }
+}
