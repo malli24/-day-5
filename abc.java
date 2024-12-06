e586c9ded497ab21ebcbc9f6f22e9adf6278be83
malli24 <malleswari82006@gmail.com>
2024-12-06 13:51:29 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,15 @@
+public class abc
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+                  
+    }
+}
