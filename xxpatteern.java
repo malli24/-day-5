37c4893ab7a964cf1f325824400d0d613857d6a1
malli24 <malleswari82006@gmail.com>
2024-12-07 10:10:22 +0530

    Add files via upload

---
+++ xxpatteern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class xxpatteern{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+        for(int j=0;j<n;j++)
+        {
+            if(i+j==n-1||i==j)
+            {
+            System.out.print("*");
+            }
+            else{
+            System.out.print(" ");
+            }
+        
+        }
+        System.out.println();
+       }
+
+        
+   }
+}
